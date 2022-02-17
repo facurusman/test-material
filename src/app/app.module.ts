86e002d95952e170cc75736fa54569413e7ab675
Facundo Rusman <facundo.rusman.04@gmail.com>
2022-02-17 18:12:12 -0300

    'Commit'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 
 import {MatToolbarModule} from '@angular/material/toolbar';
@@ -9,12 +12,27 @@ import {MatButtonModule} from '@angular/material/button';
 
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/header/header.component';
 import { ProductsComponent } from './components/products/products.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HistoryComponent } from './components/history/history.component';
+import { ErroresComponent } from './components/errores/errores.component';
+
+
+const appRoutes:Routes=[
+
+  {path:'',component:AppComponent},
+  {path:'productos', component:ProductsComponent},
+  {path:'historia', component:HistoryComponent},
+  {path:'contacto', component:ContactComponent},
+  {path:'**', component: ErroresComponent}
+
+
+]
+
+
+
 
 @NgModule({
   declarations: [
@@ -23,14 +41,20 @@ import { HistoryComponent } from './components/history/history.component';
     ProductsComponent,
     ContactComponent,
     FooterComponent,
-    HistoryComponent
+    HistoryComponent,
+    ErroresComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatButtonModule
+    MatButtonModule,
+    CommonModule,
+    [RouterModule.forRoot(appRoutes)],
+
   ],
+  exports: [RouterModule],
+
   providers: [],
   bootstrap: [AppComponent]
 })
