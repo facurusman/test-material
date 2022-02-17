3a80da397a5cb5fa32a64328780dcd604db99303
Facundo Rusman <facundo.rusman.04@gmail.com>
2022-02-17 18:49:28 -0300

    'Footer'

---
+++ src/app/components/home-componente/home-componente.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HomeComponenteComponent } from './home-componente.component';
+
+describe('HomeComponenteComponent', () => {
+  let component: HomeComponenteComponent;
+  let fixture: ComponentFixture<HomeComponenteComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ HomeComponenteComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HomeComponenteComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
