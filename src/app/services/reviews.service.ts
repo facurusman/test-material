3da9013617d4d85469f4d4233200f63b501d7d24
Facundo Rusman <facundo.rusman.04@gmail.com>
2022-03-03 17:40:31 -0300

    Agregamos la ruta para obtener los reviews, no funciona todavia

--- src/app/services/reviews.service.ts
+++ src/app/services/reviews.service.ts
@@ -10,6 +10,8 @@ export class ReviewsService {
   URL_API_REVIEW : string = ""
 
   API_KEY = '791c6dbbcbd8c0fdef30d7a5857e1135'
+  URL_API_MI_BK = 'http://localhost:3000';
+
 
 
 
@@ -18,7 +20,7 @@ export class ReviewsService {
 
 
     this.URL_API_REVIEW =  `https://api.themoviedb.org/3/movie/${id}/reviews?api_key=${this.API_KEY}&language=en-US&page=1`
-    return this.http.get( this.URL_API_REVIEW)
+    return this.http.get( this.URL_API_MI_BK)
 
   }
 }
