6ac310de037d79beb075ccec68f2a6a5435a9c3a
lucasrusman <rusmanlucas@gmail.com>
2022-03-03 09:19:01 -0300

    BE y FE conected

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -11,11 +11,11 @@ export class MoviesService {
   API_KEY = '791c6dbbcbd8c0fdef30d7a5857e1135'
 
   URL_API = `https://api.themoviedb.org/3/movie/popular?api_key=${this.API_KEY}&language=en-US`
-
+  URL_API_MI_BK = 'http://localhost:3000';
 
   getPopulars(){
 
-    return this.http.get(this.URL_API)
+    return this.http.get(this.URL_API_MI_BK)
 
   }
 
